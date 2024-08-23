bf365a1b0f1c3841bb94b757bb183c8b44718ca9
mmaarrius <mariuspopa234@gmail.com>
2024-08-23 20:05:32 +0300

    Delete scripts directory

--- scripts/fav-animations.js
+++
@@ -1,9 +0,0 @@
-export function addToCartButtonAnimation(item){
-  item.style.transform = 'scale(0.9)';
-  item.style.opacity = '0.8';
-
-  setTimeout(() => {
-    item.style.transform = 'scale(1)';
-    item.style.opacity = '1';
-  },200);
-}
